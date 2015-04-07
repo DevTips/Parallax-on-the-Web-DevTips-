54bee291a89fd349a643483d6e514e77765d4426
Andrea Leone <andrea@etrentino.it>
2015-04-07 11:02:40 +0200

    Some cleaning and adjustments

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,13 @@
 $(window).scroll(function(){
 
   var wScroll = $(this).scrollTop();
-
   $('.logo').css({
     'transform' : 'translate(0px, '+ wScroll /2 +'%)'
   });
-
   $('.back-bird').css({
     'transform' : 'translate(0px, '+ wScroll /4 +'%)'
   });
-
   $('.fore-bird').css({
-    'transform' : 'translate(0px, -'+ wScroll /55 +'%)'
+    'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
   });
-
 });

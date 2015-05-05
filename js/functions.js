4cceb8344c01cff0edc45514f4876abdd1c9264f
Andrea Leone <andrea@etrentino.it>
2015-05-05 21:11:52 +0200

    added a preloader

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", function() {
+    var load_screen = document.getElementById("load_screen");
+    document.body.removeChild(load_screen);
+});
+
 $(window).scroll(function(){
 
   var wScroll = $(this).scrollTop();

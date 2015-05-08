030f885383dab0c66027737eabe98e303cf11910
Andrea Leone <andrea@etrentino.it>
2015-05-08 13:50:42 +0200

    Episode 6 update

--- js/functions.js
+++ js/functions.js
@@ -25,4 +25,14 @@ $(window).scroll(function(){
 
   }
 
+  if(wScroll > $('.large-window').offset().top - $(window).height()){
+
+    $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
+
+    var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
+
+    $('.window-tint').css({'opacity': opacity});
+
+  }
+
 });

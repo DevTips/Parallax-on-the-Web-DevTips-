fa5b76f2fc02ed4086a16056e453003089b326d7
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2015-05-13 17:03:42 +0200

    Optimized parallax animation for better performance
    window.requestAnimationFrame, rounded pixel values and added a simple
    in view test to prevent the header to keep animating when it’s out of
    view.

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,20 @@
-$(window).scroll(function(){
-
+var parallaxScroll = function(){
   var wScroll = $(this).scrollTop();
 
-  $('.logo').css({
-    'transform' : 'translate(0px, '+ wScroll /2 +'%)'
-  });
+  // don't animate elements out of view
+  if(wScroll < $(".bird-box").height()){
+    $('.logo').css({
+      'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+    });
 
-  $('.back-bird').css({
-    'transform' : 'translate(0px, '+ wScroll /4 +'%)'
-  });
+    $('.back-bird').css({
+      'transform' : 'translate(0px, '+ wScroll /4 +'%)'
+    });
 
-  $('.fore-bird').css({
-    'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
-  });
+    $('.fore-bird').css({
+      'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
+    });
+  }
 
   if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
 
@@ -26,13 +28,17 @@ $(window).scroll(function(){
   }
 
   if(wScroll > $('.large-window').offset().top - $(window).height()){
+    // Fixed pixel value to two decimels
+    $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top).toFixed(2) +'px'});
 
-    $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
-
-    var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
+    var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5).toFixed(2);
 
     $('.window-tint').css({'opacity': opacity});
 
   }
+};
 
+
+$(window).scroll(function(){
+  window.requestAnimationFrame(parallaxScroll);
 });
